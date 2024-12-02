5a2e8dbef316679003fdd493b11af25f1d5c68e6
DraymanR <dr0548424205@gmail.com>
2024-12-02 10:58:11 +0200

    2.12.2024 1

--- src/app/component/timerComponent.tsx
+++ src/app/component/timerComponent.tsx
@@ -4,24 +4,28 @@ import { useEffect } from "react";
 import PopUpWindow from "./pop-upWindow";
 import Login from "./login";
 import useModalStore from "@/app/store/modelStore";
+import useMyUser from "../store/users";
 
 const TimerComponent = () => {
     const openModal = useModalStore((state) => state.openModal);
+    const myUserName = useMyUser((state) => state.userName);
+
     useEffect(() => {
         const token = localStorage.getItem("authToken");
 
-        if (!token) {
+        if (!myUserName) {
+            // if (!token) {
             openModal(); // הצגת המודל בתחילת השימוש
         }
 
         const intervalId = setInterval(() => {
-            if (!token) {
+            if (!myUserName) {
                 openModal(); // הפעלת פעולה כל פרק זמן מוגדר
             }
         }, 120000); // 2 דקות
 
         return () => clearInterval(intervalId); // ניקוי ה-timer בזמן הסרת הקומפוננטה
-    }, [openModal]);
+    }, []);
 
     return (
         <div>
--- src/app/services/user/registerUser.ts
+++ src/app/services/user/registerUser.ts
@@ -1,30 +1,35 @@
 import { Address, UserFormData } from '@/app/types/user';
 import axios from 'axios';
+// import axios from '';
 
 export const singIn = async (email: string, userName: string, password: string) => {
 
   const data = { email: email, password: password, username: userName }
   try {
+    console.log("data", data);
+
     const response = await axios.post('http://localhost:3000/api/users/register', data, {
       headers: {
         'Content-Type': 'application/json',
       },
     });
+    console.log(response);
 
     // החזרת התשובה מהשרת
-    return response.data;
+    return response;
   } catch (error) {
     console.error('Error registering user:', error);
     throw error; // טיפול בשגיאות
   }
 };
-export const addUser = async (data:UserFormData)=>{
+export const addUser = async (data: UserFormData) => {
   try {
     const response = await axios.post('http://localhost:3000/api/users/post', data, {
       headers: {
         'Content-Type': 'application/json',
       },
     });
+    console.log(response);
 
     // החזרת התשובה מהשרת
     return response.data;
@@ -33,3 +38,23 @@ export const addUser = async (data:UserFormData)=>{
     throw error; // טיפול בשגיאות
   }
 };
+
+export const getMyDetails = async (username: string) => {
+  try {
+
+    // const myUserNAme = localStorage.getItem("")
+    const response = await axios.get(`http://localhost:3000/api/users/get/username?username=${username}`, {
+      withCredentials: true,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    console.log('User :', response.data);
+    return response.data
+  } catch (error) {
+    console.error('Error registering user:', error);
+    throw error; // טיפול בשגיאות
+  }
+};
+
+// checkAuth();
