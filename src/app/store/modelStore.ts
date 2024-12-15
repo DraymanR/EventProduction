260bacff5e643d3d653456e40262bee6017cc785
shoshana <shoshgrilak@gmail.com>
2024-12-15 14:13:41 +0200

    merge

--- src/app/store/modelStore.ts
+++ src/app/store/modelStore.ts
@@ -13,10 +13,6 @@ const useModalStore = create<ModalState>((set) => ({
   closeModal: () => set({ isModalOpen: false }),
 }));
 
-<<<<<<< HEAD
-export default useModalStore;
-=======
 export default useModalStore;
 
 
->>>>>>> 2ebe7af3aec6debd6fbbf4f07105abe8a5f0d7dc
