7197c883bbcf1453e7776b424985cd1b883bb587
SariGefner <saragefner8008@gmail.com>
2024-12-16 14:21:18 +0200

    commit before merge sari2 into main

--- src/app/api/users/register/newPassword/route.ts
+++ src/app/api/users/register/newPassword/route.ts
@@ -1,18 +1,6 @@
 
 import { NextResponse } from 'next/server';
 import bcrypt from 'bcryptjs';
-<<<<<<< HEAD
-import jwt from 'jsonwebtoken';
-import { AuthModel } from '@/app/lib/models/user';
-import { UserModel } from '@/app/lib/models/user'; // import the User model
-import connectDb from '@/app/lib/db/connectDb';
-
-const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key';
-
-export async function POST(req: Request) {
-    try {
-        const { email, otp, newPassword } = await req.json(); 
-=======
 import { AuthModel, UserModel } from '@/app/lib/models/user';
 import connectDb from '@/app/lib/db/connectDb';
 import { generateToken, setAuthCookies } from '@/middlewares/authMiddleware';
@@ -20,7 +8,6 @@ import { generateToken, setAuthCookies } from '@/middlewares/authMiddleware';
 export async function POST(req: Request) {
     try {
         const { email, otp, newPassword } = await req.json();
->>>>>>> feb4b53c36ceefe34479e7431dd7d5b0453e91d4
 
         if (!email || !otp || !newPassword) {
             return NextResponse.json(
@@ -31,11 +18,8 @@ export async function POST(req: Request) {
 
         await connectDb();
 
-<<<<<<< HEAD
         // חפש את המשתמש לפי האימייל במודל Auth
-=======
       
->>>>>>> feb4b53c36ceefe34479e7431dd7d5b0453e91d4
         const authRecord = await AuthModel.findOne({ email });
 
         if (!authRecord) {
@@ -45,10 +29,7 @@ export async function POST(req: Request) {
             );
         }
 
-<<<<<<< HEAD
-=======
        
->>>>>>> feb4b53c36ceefe34479e7431dd7d5b0453e91d4
         const user = await UserModel.findOne({ email });
 
         if (!user) {
@@ -58,60 +39,38 @@ export async function POST(req: Request) {
             );
         }
 
-<<<<<<< HEAD
-        const userName = user.userName;  
-
-       
-        if (authRecord.otp != otp || new Date() > authRecord.otpExpiration) {
-=======
         const userName = user.userName;
 
         
         if (authRecord.otp !== otp || new Date() > authRecord.otpExpiration) {
->>>>>>> feb4b53c36ceefe34479e7431dd7d5b0453e91d4
             return NextResponse.json(
                 { error: 'Invalid or expired OTP' },
                 { status: 400 }
             );
         }
 
-<<<<<<< HEAD
-     
-        const hashedPassword = await bcrypt.hash(newPassword, 10);
-        // console.log('Old Password:', authRecord.password);
-        authRecord.password = hashedPassword;
-        // console.log('New Password:', authRecord.password);
-        // await authRecord.save();
-        // console.log('Password updated successfully');
-        
-=======
        
         const hashedPassword = await bcrypt.hash(newPassword, 10);
->>>>>>> feb4b53c36ceefe34479e7431dd7d5b0453e91d4
         authRecord.password = hashedPassword;
         authRecord.otp = null;
         authRecord.otpExpiration = null;
         await authRecord.save();
 
-<<<<<<< HEAD
       
-        const payload = { userName, email };
+        // const payload = { userName, email };
 
         
-        const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
+        // const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
 
       
-=======
         const token = generateToken({ userName, email });
 
         
->>>>>>> feb4b53c36ceefe34479e7431dd7d5b0453e91d4
         const response = NextResponse.json(
             { message: 'Password successfully updated', userName },
             { status: 200 }
         );
 
-<<<<<<< HEAD
         response.cookies.set('userName', userName, {
             httpOnly: false,
             secure: process.env.NODE_ENV === 'production',
@@ -125,9 +84,7 @@ export async function POST(req: Request) {
             maxAge: 3600,
             path: '/',
         });
-=======
         setAuthCookies(response, userName, token);
->>>>>>> feb4b53c36ceefe34479e7431dd7d5b0453e91d4
 
         return response;
 
