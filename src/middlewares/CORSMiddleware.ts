0cf5bbf84e5a2f21c2d840e16e0f671757d39c34
DraymanR <dr0548424205@gmail.com>
2024-12-19 11:33:07 +0200

    add cors middleware

---
+++ src/middlewares/CORSMiddleware.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request:NextRequest) {
+  const response = NextResponse.next();
+
+  // Add CORS headers to the response
+  response.headers.set('Access-Control-Allow-Origin', 'https://event-production-fawn.vercel.app');
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  // Handle preflight request
+  if (request.method === 'OPTIONS') {
+    return new Response(null, {
+      headers: response.headers,
+    });
+  }
+
+  return response;
+}
