5fe2ab9e0bed7e70c9b4c4d4c683b9c4bc02f3b1
RivkaKaminski <rivka.kaminski@gmail.com>
2024-12-24 14:43:23 +0200

    Your commit message

--- src/app/component/posts/PostCard.tsx
+++ src/app/component/posts/PostCard.tsx
@@ -1,13 +1,16 @@
-'use client';
+"use client";
 
 import React, { useState } from "react";
 import { PostCardProps, Recommendation } from "@/app/types/post";
-import { addingMyFavoritePost, addRecommendation } from "@/app/services/post/post"; // ייבוא הפונקציה
+import {
+  addingMyFavoritePost,
+  addRecommendation,
+} from "@/app/services/post/post"; // ייבוא הפונקציה
 import Image from "next/image";
 
 import { Link } from "react-router-dom";
 import PostModal from "./PostModel";
-
+import PopUpWindow from "../pop-upWindow";
 
 const PostCard: React.FC<{ post: PostCardProps }> = ({ post }) => {
   const [showPostModal, setShowPostModal] = useState(false);
@@ -23,32 +26,48 @@ const PostCard: React.FC<{ post: PostCardProps }> = ({ post }) => {
   return (
     <div className="relative">
       {/* כרטיס הפוסט */}
-      <div className={`post-card bg-white rounded-lg shadow-md p-6 max-w-xl mx-auto mb-6 ${showPostModal ? "filter blur-sm" : ""}`}>
+      <div
+        className={`post-card bg-white rounded-lg shadow-md p-6 max-w-xl mx-auto mb-6 ${
+          showPostModal ? "filter blur-sm" : ""
+        }`}
+      >
         <div className="header flex items-center justify-between mb-4">
           <h2 className="text-2xl font-semibold text-gray-800">{post.title}</h2>
-    
+
           <p className="text-sm text-gray-500">
-            <a href={`/pages/users/${post.userName}`} className="text-gray-600 hover:text-red-400">
+            <a
+              href={`/pages/users/${post.userName}`}
+              className="text-gray-600 hover:text-red-400"
+            >
               {post.userName}
             </a>
           </p>
         </div>
         <p className="description text-gray-700 mb-4">{post.description}</p>
 
-        <div className="comments-toggle flex items-center text-blue-500 cursor-pointer" onClick={openPostModal}>
+        <div
+          className="comments-toggle flex items-center text-blue-500 cursor-pointer"
+          onClick={openPostModal}
+        >
           <span> הראה פוסט מלא</span>
-          <span className={`ml-2 transform ${showPostModal ? "rotate-180" : ""}`}>➔</span>
+          <span
+            className={`ml-2 transform ${showPostModal ? "rotate-180" : ""}`}
+          >
+            ➔
+          </span>
         </div>
       </div>
 
       {/* מודל להצגת פוסט מלא */}
+
       {showPostModal && (
         <div className="fixed inset-0 bg-gray-500 bg-opacity-50 flex justify-center items-center z-50">
           <div className="modal-content bg-white p-6 rounded-md max-w-xl shadow-lg overflow-y-auto max-h-[80vh]">
-            <PostModal post={post} />
-            {/* //onClose={closePostModal} /> */}
+          <button onClick={closePostModal} className="text-red-500">
+              X
+            </button>
 
-            {/* כפתור סגירה */}
+            <PostModal post={post} />
           </div>
         </div>
       )}
--- src/app/pages/user-account/my-events/page.tsx
+++ src/app/pages/user-account/my-events/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import AddPost from "@/app/component/posts/AddPost";
 import PopUpWindow from "@/app/component/pop-upWindow";
@@ -6,38 +6,47 @@ import { Post, PostCardProps } from "@/app/types/post";
 import { useState } from "react";
 import useUserStore from "@/app/store/userModel";
 import PostCard from "@/app/component/posts/PostCard";
-import '@/app/globals.css'
+import "@/app/globals.css";
 import useModalStore from "@/app/store/modelStore";
 
 const Home: React.FC = () => {
-    const openModal = useModalStore((state: { openModal: any; }) => state.openModal);
-    const isModalOpen = useModalStore((state: { isModalOpen: any; }) => state.isModalOpen);
-    const postArr = useUserStore((state) => state.postArr);
-    const [MyEvents, setMyEvents] = useState<PostCardProps[]>(postArr);
+  const openModal = useModalStore(
+    (state: { openModal: any }) => state.openModal
+  );
+  const isModalOpen = useModalStore(
+    (state: { isModalOpen: any }) => state.isModalOpen
+  );
+  const postArr = useUserStore((state) => state.postArr);
+  const [MyEvents, setMyEvents] = useState<PostCardProps[]>(postArr);
 
-    const handleAddEvent = () => {
-        if (!isModalOpen) {
-            openModal();
-        }
-    };
+  const handleAddEvent = () => {
+    if (!isModalOpen) {
+      openModal();
+    }
+  };
 
+  return (
+    <div dir="ltr">
+      <PopUpWindow>
+        <AddPost></AddPost>
+      </PopUpWindow>
 
-    return (
-        <div dir="ltr">
-            <PopUpWindow>
-                <AddPost ></AddPost>
-            </PopUpWindow>
+      <div className="space-y-6 mt-4">
+        <h2 className="page-title">:האירועים שלי</h2>
+        {MyEvents.map((post: PostCardProps, index: number) => {
+          // המרת הפוסט
 
-            <div className="space-y-6 mt-4">
-                <h2 className="page-title">:האירועים שלי</h2>
-                {MyEvents.map((post: PostCardProps, index: number) => {
-                  // המרת הפוסט
-                    return <PostCard key={index} post={post} />;
-                })}
-            </div>
-            <button type="button" onClick={() => handleAddEvent()} className="button-primary">הוספת אירוע</button>
-
-        </div>
-    )
-}
+          return <PostCard key={index} post={post} />;
+        })}
+      </div>
+      <button
+        type="button"
+        onClick={() => handleAddEvent()}
+        className="button-primary"
+      >
+        הוספת אירוע
+      </button>
+    </div>
+  );
+};
 export default Home;
