2ee84c8d5e5ab096b4248e327f3756bea06bbc2e
RivkaKaminski <rivka.kaminski@gmail.com>
2024-12-26 11:39:25 +0200

    organize popup

--- src/app/component/users/register/login.tsx
+++ src/app/component/users/register/login.tsx
@@ -1,54 +1,73 @@
-'use client'
-import React, { useState } from 'react';
-import LoginWithPassword from '@/app/component/users/register/loginWithPassword';
-import EnterCodeFromEmail from '@/app/component/users/register/enterCodeFromEmail';
-import { forgetPassword } from '@/app/services/user/registerUser';
-import Register from './register';
-import ResetPassword from '@/app/component/users/register/newPassword';
-import '@/app/globals.css'
+"use client";
 
+import React, { useState } from "react";
+import LoginWithPassword from "@/app/component/users/register/loginWithPassword";
+import EnterCodeFromEmail from "@/app/component/users/register/enterCodeFromEmail";
+import { forgetPassword } from "@/app/services/user/registerUser";
+import Register from "./register";
+import ResetPassword from "@/app/component/users/register/newPassword";
+import "@/app/globals.css";
 
 const Login: React.FC = () => {
-    const [currentStep, setCurrentStep] = useState<'LoginWithPassword' | 'newPassword' | 'EnterCodeFromEmail' | 'newUser'>('LoginWithPassword');
-    const [errorMessage, setErrorMessage] = useState('');
-    const [codeFromEmail, setCodeFromEmail] = useState('');
-    const [userEmail, setUserFromEmail] = useState('');
+  const [currentStep, setCurrentStep] = useState<
+    "LoginWithPassword" | "newPassword" | "EnterCodeFromEmail" | "newUser"
+  >("LoginWithPassword");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [codeFromEmail, setCodeFromEmail] = useState("");
+  const [userEmail, setUserFromEmail] = useState("");
 
+  const handleForgetPassword = async (email: string) => {
+    try {
+      const result = await forgetPassword(email);
+      setUserFromEmail(email);
+      console.log(result);
+      setErrorMessage("");
+      setCurrentStep("EnterCodeFromEmail");
+    } catch (error) {
+      setErrorMessage("Failed to send the code.");
+      console.error(error);
+    }
+  };
 
-    const handleForgetPassword = async (email: string) => {
-        // setErrorMessage('');
-        try {
-            const result = await forgetPassword(email);
-            setUserFromEmail(email)
-            console.log(result);
-            // setCodeFromEmail(result.OTP);
-            setErrorMessage('');
-            setCurrentStep('EnterCodeFromEmail');
-        } catch (error) {
-            setErrorMessage('Failed to send the code.');
-            console.error(error);
-
-        }
-    };
-
-    return (
-        <div>
-            {currentStep === 'LoginWithPassword' && (
-                <LoginWithPassword onForgetPassword={handleForgetPassword} onNewUser={() => { setErrorMessage(''); setCurrentStep('newUser') }} />
-            )}
-            {currentStep == 'EnterCodeFromEmail' && (
-                <EnterCodeFromEmail onBack={(currentStep: 'LoginWithPassword' | 'newPassword', myCodeFromEmail: string) => { setCodeFromEmail(myCodeFromEmail); setErrorMessage(''); setCurrentStep(currentStep) }} />
-            )}
-            {currentStep == 'newPassword' && (
-                <ResetPassword otp={codeFromEmail} email={userEmail} />
-            )}
-            {currentStep === 'newUser' && (
-                <Register onBack={() => { setErrorMessage(''); setCurrentStep('LoginWithPassword') }} />
-            )}
-            {errorMessage && <p className="error-message">{errorMessage}</p>}
-
-        </div>
-    );
+  return (
+    <div className="login-wrapper">
+      <div className="login-container">
+        {currentStep === "LoginWithPassword" && (
+          <LoginWithPassword
+            onForgetPassword={handleForgetPassword}
+            onNewUser={() => {
+              setErrorMessage("");
+              setCurrentStep("newUser");
+            }}
+          />
+        )}
+        {currentStep === "EnterCodeFromEmail" && (
+          <EnterCodeFromEmail
+            onBack={(
+              currentStep: "LoginWithPassword" | "newPassword",
+              myCodeFromEmail: string
+            ) => {
+              setCodeFromEmail(myCodeFromEmail);
+              setErrorMessage("");
+              setCurrentStep(currentStep);
+            }}
+          />
+        )}
+        {currentStep === "newPassword" && (
+          <ResetPassword otp={codeFromEmail} email={userEmail} />
+        )}
+        {currentStep === "newUser" && (
+          <Register
+            onBack={() => {
+              setErrorMessage("");
+              setCurrentStep("LoginWithPassword");
+            }}
+          />
+        )}
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+      </div>
+    </div>
+  );
 };
 
 export default Login;
