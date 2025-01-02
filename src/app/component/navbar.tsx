e003bfe53e4d037ddf11bd6c8d770a612cd30cda
SariGefner <saragefner8008@gmail.com>
2025-01-02 15:27:11 +0200

    add logo

--- src/app/component/navbar.tsx
+++ src/app/component/navbar.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import profileImage from '@/app/assets/images/defaultConsumerProfile.png';
 import Image from 'next/image';
 import { checkIfLoggedIn } from '@/app/services/user/registerUser';
@@ -6,7 +7,6 @@ import useNavbarStore from '../store/navbarStore';
 import useUserStore from '../store/userModel';
 import useModalStore from '../store/modelStore';
 import Logo from '@/app/assets/images/logo.png';
-
 import { useState } from 'react';
 
 const Navbar = () => {
@@ -17,7 +17,7 @@ const Navbar = () => {
 
   const handleProfileClick = () => {
     if (checkIfLoggedIn()) {
-      setOpenSideBar(!openSideBar)
+      setOpenSideBar(!openSideBar);
       toggleNavbar(openSideBar);
     } else {
       openModal();
@@ -27,9 +27,21 @@ const Navbar = () => {
   return (
     <div className="fixed top-0 left-0 right-0 w-full bg-[#6C48C5] shadow-md z-50 h-[105px]">
       <nav className="flex justify-between items-center p-4 max-w-screen-2xl mx-auto relative h-full">
-        {/* Profile Section - Right aligned but vertically centered */}
+        {/* Logo - Now positioned on the left */}
+        <div className="order-2">
+          <Image
+            src={Logo}
+            alt="חגיגה מושלמת Logo"
+            height={48}
+            width={130}
+            priority
+            className="hover:opacity-90 transition-opacity"
+          />
+        </div>
+
+        {/* Profile Section - Remains on the right */}
         <div
-          className="absolute right-4 top-1/2 -translate-y-1/2 flex flex-col items-center cursor-pointer"
+          className="order-1 flex flex-col items-center cursor-pointer"
           onClick={handleProfileClick}
         >
           {checkIfLoggedIn() && userDetails?.profileImage ? (
@@ -51,7 +63,9 @@ const Navbar = () => {
           )}
           <div className="text-center">
             {checkIfLoggedIn() && userDetails ? (
-              <span className="text-[#101f61] text-sm font-medium font-bold" >{userDetails.firstName} {userDetails.lastName}</span>
+              <span className="text-[#101f61] text-sm font-medium font-bold">
+                {userDetails.firstName} {userDetails.lastName}
+              </span>
             ) : (
               <span className="text-[#101f61] text-sm font-medium font-bold">הפרופיל שלי</span>
             )}
