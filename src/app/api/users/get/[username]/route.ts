e6c0ac49c650800078669de9c4abfa164a76ba36
shoshana <shoshgrilak@gmail.com>
2025-01-05 09:49:58 +0200

    vercel

--- src/app/api/users/get/[username]/route.ts
+++ src/app/api/users/get/[username]/route.ts
@@ -1,135 +1,10 @@
-// import { NextResponse, NextRequest } from 'next/server';
-// import { UserModel, SupplierModel } from '@/app/lib/models/user';
-// import { User, Title } from '@/app/types/user';
-// import connectDb from '@/app/lib/db/connectDb';
-// import { verifyTokenMiddleware } from '@/middlewares/middlewareToken'; // נניח שהמיקום של ה-middleware
-
-
-
-// export async function GET(req: NextRequest) {
-//     try {
-//         await connectDb();
-//         let userName: string | undefined;
-
-//         // השתמש במידלוואר
-//         await verifyTokenMiddleware(req as any, {} as NextResponse, () => {
-//             userName = (req as any).userName; // קבלת userName ממידלוואר
-//         });
-
-//         const { searchParams } = new URL(req.url);
-//         const userNameFromQuery = searchParams.get('username');
-
-//         if (!userNameFromQuery) {
-//             return NextResponse.json(
-//                 { error: 'Missing username' },
-//                 { status: 400 }
-//             );
-//         }
-//         console.log(userNameFromQuery);
-
-//         const user = await UserModel.findOne({ userName: userNameFromQuery })
-//             .populate('addressId')
-//             .populate({
-//                 path: 'postArr',
-//                 populate: [
-//                     {
-//                         path: 'recommendations',
-//                         model: 'Recommendation',
-//                     },
-//                     {
-//                         path: 'postId', // זה ה-ID של ה-ConsumerPost
-//                         model: 'ConsumerPost', // הכוונה היא לפופל את ה-ID של ה-ConsumerPost
-//                     },
-//                 ]
-//             }).populate({
-//                 path: 'likedPostsArr',
-//                 populate: [{
-//                     path: 'recommendations',
-//                     model: 'Recommendation',
-//                 }, {
-//                     path: 'postId',
-//                     model: 'ConsumerPost',
-//                 }]
-//             })
-//             .lean<User>();
-
-
-//         console.log("user", user);
-
-
-//         if (!user) {
-//             return NextResponse.json(
-//                 { error: 'User not found' },
-//                 { status: 404 }
-//             );
-//         }
-//         let supplierDetails;
-
-//         if (
-//             user.titles.some((title) =>
-//                 Object.values(Title).includes(title as Title)
-//             )
-//         ) {
-//             supplierDetails = await SupplierModel.findOne({ userName: userNameFromQuery }).lean();
-//         }
-
-//         if (user.userName !== userName) {
-
-//             const { firstName, lastName, addressId, likedPeople, likedPostsArr, ...filteredUser } = user;
-//             return NextResponse.json(
-//                 {
-//                     message: 'User retrieved successfully',
-//                     user: filteredUser,
-//                     supplierDetails,
-
-//                 },
-//                 {
-//                     status: 200,
-//                     headers: {
-//                         'Access-Control-Allow-Credentials': 'true',
-//                         'Access-Control-Allow-Origin': process.env.NODE_ENV === 'production'
-//                             ? 'https://event-production-fawn.vercel.app'
-//                             : 'http://localhost:3000',
-//                         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-//                         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-//                     }
-//                 }
-//             );
-//         }
-
-//         return NextResponse.json(
-//             {
-//                 message: 'User retrieved successfully',
-//                 user,
-
-//             },
-//             {
-//                 status: 200,
-//                 headers: {
-//                     'Access-Control-Allow-Credentials': 'true',
-//                     'Access-Control-Allow-Origin': process.env.NODE_ENV === 'production'
-//                         ? 'https://event-https://event-production-fawn.vercel.app-git-main-riva-draimans-projects.vercel.app'
-//                         : 'http://localhost:3000',
-//                     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-//                     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-//                 }
-//             }
-//         );
-//     } catch (error) {
-//         console.error(error);
-
-//         return NextResponse.json(
-//             { error: 'Internal Server Error' },
-//             { status: 500 }
-//         );
-//     }
-// }
-
 import { NextResponse, NextRequest } from 'next/server';
 import { UserModel, SupplierModel } from '@/app/lib/models/user';
 import { User, Title } from '@/app/types/user';
 import connectDb from '@/app/lib/db/connectDb';
-import { verifyTokenMiddleware } from '@/middlewares/middlewareToken';
+import { verifyTokenMiddleware } from '@/middlewares/middlewareToken'; // נניח שהמיקום של ה-middleware
+
+
 
 export async function GET(req: NextRequest) {
     try {
@@ -150,6 +25,7 @@ export async function GET(req: NextRequest) {
                 { status: 400 }
             );
         }
+        console.log(userNameFromQuery);
 
         const user = await UserModel.findOne({ userName: userNameFromQuery })
             .populate('addressId')
@@ -161,12 +37,25 @@ export async function GET(req: NextRequest) {
                         model: 'Recommendation',
                     },
                     {
-                        path: 'postId',
-                        model: 'ConsumerPost',
+                        path: 'postId', // זה ה-ID של ה-ConsumerPost
+                        model: 'ConsumerPost', // הכוונה היא לפופל את ה-ID של ה-ConsumerPost
                     },
                 ]
+            }).populate({
+                path: 'likedPostsArr',
+                populate: [{
+                    path: 'recommendations',
+                    model: 'Recommendation',
+                }, {
+                    path: 'postId',
+                    model: 'ConsumerPost',
+                }]
             })
             .lean<User>();
+
+
+        console.log("user", user);
+
 
         if (!user) {
             return NextResponse.json(
@@ -174,7 +63,6 @@ export async function GET(req: NextRequest) {
                 { status: 404 }
             );
         }
-
         let supplierDetails;
 
         if (
@@ -186,19 +74,21 @@ export async function GET(req: NextRequest) {
         }
 
         if (user.userName !== userName) {
+
             const { firstName, lastName, addressId, likedPeople, likedPostsArr, ...filteredUser } = user;
             return NextResponse.json(
                 {
                     message: 'User retrieved successfully',
                     user: filteredUser,
                     supplierDetails,
+
                 },
                 {
                     status: 200,
                     headers: {
                         'Access-Control-Allow-Credentials': 'true',
                         'Access-Control-Allow-Origin': process.env.NODE_ENV === 'production'
-                            ? 'https://your-vercel-app-url.vercel.app'  // כתובת ה-URL של ה-Vercel שלך
+                            ? 'https://event-production-fawn.vercel.app'
                             : 'http://localhost:3000',
                         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
                         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
@@ -211,13 +101,14 @@ export async function GET(req: NextRequest) {
             {
                 message: 'User retrieved successfully',
                 user,
+
             },
             {
                 status: 200,
                 headers: {
                     'Access-Control-Allow-Credentials': 'true',
                     'Access-Control-Allow-Origin': process.env.NODE_ENV === 'production'
-                        ? 'https://your-vercel-app-url.vercel.app'  // כתובת ה-URL של ה-Vercel שלך
+                        ? 'https://event-production-fawn.vercel.app'
                         : 'http://localhost:3000',
                     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
                     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
